9a907205fbc41450c8eaa83ab27e2fd61ddd3195
QuentinProulx <quentin.proulx@gmail.com>
2025-06-15 04:23:34 -0400

    Reimplemented the direction vector for entities

--- src/main/java/Entities/Entity.java
+++ src/main/java/Entities/Entity.java
@@ -11,6 +11,7 @@ public abstract class Entity {
     public static List<Entity> entities = new ArrayList<Entity>();
     public Matrix position;
     public Matrix rotations;
+    public Matrix forward;
 
     public int debugId;
     public static int nextDebugId = 0;
@@ -19,16 +20,28 @@ public abstract class Entity {
         this.debugId = nextDebugId++;
 
         position = new Matrix(new double[][]{{x}, {y}, {z}});
-
         rotations = new Matrix(new double[][]{{dirX}, {dirY}, {dirZ}});
+
+        forward = new Matrix(new double[][]{{0}, {1}, {0}});
+        forward
+                .rotateX3(dirX, new Matrix(new double[][]{{0}, {0}, {0}}))
+                .rotateY3(dirY, new Matrix(new double[][]{{0}, {0}, {0}}))
+                .rotateZ3(dirZ, new Matrix(new double[][]{{0}, {0}, {0}}));
     }
 
     public Entity(Matrix position, Matrix rotations) {
         this.position = position;
         this.rotations = rotations;
+
+        forward = new Matrix(new double[][]{{0}, {1}, {0}});
+        forward
+                .rotateX3(this.rotations.info[0][0], new Matrix(new double[][]{{0}, {0}, {0}}))
+                .rotateY3(this.rotations.info[1][0], new Matrix(new double[][]{{0}, {0}, {0}}))
+                .rotateZ3(this.rotations.info[2][0], new Matrix(new double[][]{{0}, {0}, {0}}));
     }
 
     public void move(double x, double y, double z) {
+        Matrix center = new Matrix(new double[][]{{0}, {0}, {0}});
         Matrix rotate = new Matrix(new double[][]{{x}, {y}, {z}});
 
         position = position.add(rotate)
@@ -44,6 +57,7 @@ public abstract class Entity {
 
         this.rotations.info[0][0] += angle;
         this.position = this.position.rotateX3(angle, origin);
+        this.forward = this.forward.rotateX3(angle, origin);
     }
 
     public void rotateY(double angle, Matrix origin) {
@@ -53,6 +67,7 @@ public abstract class Entity {
 
         this.rotations.info[1][0] += angle;
         this.position = this.position.rotateY3(angle, origin);
+        this.forward = this.forward.rotateY3(angle, origin);
     }
 
     public void rotateZ(double angle, Matrix origin) {
@@ -62,5 +77,6 @@ public abstract class Entity {
 
         this.rotations.info[2][0] += angle;
         this.position = this.position.rotateZ3(angle, origin);
+        this.forward = this.forward.rotateZ3(angle, origin);
     }
 }
